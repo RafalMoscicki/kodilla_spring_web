d6c82d16f7e9b5b365929a0dcf0dc0a0f9ee0725
RafalMoscicki <60443383+RafalMoscicki@users.noreply.github.com>
2021-09-22 19:27:41 +0200

    KOD.15.2. findById Crud repo (#2)

---
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -0,0 +1,25 @@
+package com.crud.tasks.service;
+
+import com.crud.tasks.domain.Task;
+import com.crud.tasks.repository.TaskRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DbService {
+
+    private final TaskRepository repository;
+
+    public DbService(TaskRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Task> getAllTasks() {
+        return repository.findAll();
+    }
+
+    public Task findTaskById(Long id) {
+        return repository.findById(id).orElse(null);
+    }
+}
